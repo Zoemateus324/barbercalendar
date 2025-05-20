05f719f122d27d4cccc3077fb2c73167289096a9
Zoé Mateus <zmmateus2@gmail.com>
2025-05-20 09:37:10 -0300

    chore: create a banner and configuration models

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
 import Header from "@/components/Header";
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
+import BannerBarber from '../../public/banner-01.png'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <Header/>
+      <Header />
+      <div className="p-5">
+
+        <h2 className="text-xl font-bold">Olá, Zoé</h2>
+        <p>Terça-feira, 20 de maio</p>
+
+        <div className="flex items-center gap-2 mt-6">
+
+        <Input placeholder="Faça sua busca..."/>
+        <Button>
+          <SearchIcon/>
+        </Button>
+        </div>
+       <div className="relative mt-6 w-full h-[150px] rounded-xl">
+         <Image 
+        src={BannerBarber}
+        alt="banner barber"
+        fill className="object-cover"
+        />
+       </div>
+      </div>
+
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import logoBarber from '../../public/logo.png'
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
 const Header = () => {
     return ( 
         <Card>
-            <CardContent>
+            <CardContent className="p-5 justify-between flex flex-items items-center">
                 <Image
                 src={logoBarber}
                 alt="Logo Barber"
+                height={18}
+                width={120}
                 />
+                <Button size="icon" variant="outline">
+                    <MenuIcon/>
+                </Button>
             </CardContent>
         </Card>
      );
