89758297dc76a1aef61d8d6470c455c409040912
Zoé Mateus <zmmateus2@gmail.com>
2025-05-20 10:01:30 -0300

    chore: create a component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
 import BannerBarber from '../../public/banner-01.png'
 import Image from "next/image";
+import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
   return (
@@ -28,6 +31,23 @@ export default function Home() {
         fill className="object-cover"
         />
        </div>
+      <div className="flex">
+         <Card className="mt-6">
+        <CardContent className="flex
+        ">
+      <div className="flex flex-col gap-2 py-5 pl-5">
+        <Badge>Confirmado</Badge>
+        <h3 className="">Corte de Cabelo</h3>
+        <div className="flex items-center gap-2">
+          <Avatar className="h-6 w-6">
+            <AvatarImage src="https://cdn-icons-png.flaticon.com/512/219/219983.png" />
+          </Avatar>
+          <p className="text-sm">Barbearia Full Stack</p>
+        </div>
+      </div>
+        </CardContent>
+       </Card>
+      </div>
       </div>
 
     </div>
