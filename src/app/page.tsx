ed1169b3057633e487c526a842e5fbf481596234
Zoé Mateus <zmmateus2@gmail.com>
2025-05-20 13:59:13 -0300

    chore:update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,38 +16,45 @@ export default function Home() {
 
         <h2 className="text-xl font-bold">Olá, Zoé</h2>
         <p>Terça-feira, 20 de maio</p>
-
+{/* Banner */}
         <div className="flex items-center gap-2 mt-6">
 
-        <Input placeholder="Faça sua busca..."/>
-        <Button>
-          <SearchIcon/>
-        </Button>
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
         </div>
-       <div className="relative mt-6 w-full h-[150px] rounded-xl">
-         <Image 
-        src={BannerBarber}
-        alt="banner barber"
-        fill className="object-cover"
-        />
-       </div>
-      <div className="flex">
-         <Card className="mt-6">
-        <CardContent className="flex
+        <div className="relative mt-6 w-full h-[150px] rounded-xl">
+          <Image
+            src={BannerBarber}
+            alt="banner barber"
+            fill className="object-cover"
+            />
+        </div>
+          <Card className="mt-6">
+            <CardContent className="flex justify-between p-0
         ">
-      <div className="flex flex-col gap-2 py-5 pl-5">
-        <Badge>Confirmado</Badge>
-        <h3 className="">Corte de Cabelo</h3>
-        <div className="flex items-center gap-2">
-          <Avatar className="h-6 w-6">
-            <AvatarImage src="https://cdn-icons-png.flaticon.com/512/219/219983.png" />
-          </Avatar>
-          <p className="text-sm">Barbearia Full Stack</p>
-        </div>
-      </div>
-        </CardContent>
-       </Card>
-      </div>
+          {/* Card Esquerda */}
+              <div className="flex flex-col gap-2 py-5 pl-5">
+                <Badge className="w-fit ">Confirmado</Badge>
+                <h3 className="">Corte de Cabelo</h3>
+                <div className="flex items-center gap-2">
+                  <Avatar className="h-6 w-6">
+                    <AvatarImage src="https://cdn-icons-png.flaticon.com/512/219/219983.png" />
+                  </Avatar>
+                  <p className="text-sm">Barbearia Full Stack</p>
+                </div>
+              </div>
+
+          {/* Card Direita */}
+              <div className="flex flex-col items-center justify-center">
+                <p className="text-sm">Maio</p>
+                <p className="text-2xl">20</p>
+                <p className="text-sm">10:17</p>
+              </div>
+            </CardContent>
+          </Card>
+       
       </div>
 
     </div>
