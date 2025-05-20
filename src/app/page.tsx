642b69b1f2b3681f72aaa4c95a17222192310fd4
Zoé Mateus <zmmateus2@gmail.com>
2025-05-20 16:18:17 -0300

    chore: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,22 @@ import Image from "next/image";
 import { Card, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import BarberShopItem from "@/components/Barbershopitem";
+import { db } from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home () {
+  const barbershops = await db.barbershop.findMany({})
+ 
+
+
   return (
-    <div className="">
+    <div >
       <Header />
       <div className="p-5">
 
         <h2 className="text-xl font-bold">Olá, Zoé</h2>
         <p>Terça-feira, 20 de maio</p>
-{/* Banner */}
+        {/* Banner */}
         <div className="flex items-center gap-2 mt-6">
 
           <Input placeholder="Faça sua busca..." />
@@ -29,35 +35,45 @@ export default function Home() {
             src={BannerBarber}
             alt="banner barber"
             fill className="object-cover"
-            />
+          />
         </div>
-            {/* Card Esquerda */}
-            <h2 className="font-bold uppercase mb-3 mt-6 text-xs text-gray-500 ">AGENDAMENTOS</h2>
-          <Card className="">
-            <CardContent className="flex justify-between p-2
+        {/* Card Esquerda */}
+        <h2 className="font-bold uppercase mb-3 mt-6 text-xs text-gray-500 ">AGENDAMENTOS</h2>
+        <Card className="">
+          <CardContent className="flex justify-between p-2
         ">
-              <div className="flex flex-col gap-2 py-5 pl-5">
-                <Badge className="w-fit ">Confirmado</Badge>
-                <h3 className="">Corte de Cabelo</h3>
-                <div className="flex items-center gap-2">
-                  <Avatar className="h-6 w-6">
-                    <AvatarImage src="https://cdn-icons-png.flaticon.com/512/219/219983.png" />
-                  </Avatar>
-                  <p className="text-sm">Barbearia Full Stack</p>
-                </div>
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit ">Confirmado</Badge>
+              <h3 className="">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://cdn-icons-png.flaticon.com/512/219/219983.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia Full Stack</p>
               </div>
+            </div>
 
-          {/* Card Direita */}
-              <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-                <p className="text-sm">Maio</p>
-                <p className="text-2xl">20</p>
-                <p className="text-sm">10:17</p>
-              </div>
-            </CardContent>
-          </Card>
-       
+            {/* Card Direita */}
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Maio</p>
+              <p className="text-2xl">20</p>
+              <p className="text-sm">10:17</p>
+            </div>
+          </CardContent>
+        </Card>
+        
+        {/* Recomendações */}
+        <h2 className="font-bold uppercase mb-3 mt-6 text-xs text-gray-500">Recomendados</h2>
+      
+      <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+
+      {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} {...barbershop} />
+          ))}
       </div>
-
+      
+      
+      </div>
     </div>
   );
 }
--- src/components/Barbershopitem.tsx
+++ src/components/Barbershopitem.tsx
@@ -1,6 +1,35 @@
-const BaberShopItem = () => {
+
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+
+type Barbershop = {
+    name: string;
+    address: string;
+    imageUrl: string;
+    // Add other properties as needed
+};
+
+const BaberShopItem = (barbershop: Barbershop) => {
     return ( 
-        <h1>Recomendados</h1>
+        <div>
+
+        <Card className="min-w-[167px] roudended-2xl">
+            <CardContent className="p-0">
+                
+                {/* Imagem */}
+                <div className="relative h-[159px] w-[159px]">
+                    <Image fill className="object-conver roudend-2xl" src={barbershop.imageUrl} alt={barbershop.name}/>
+                    {/* TExt */}
+                </div>
+                    <div className="px-2 py-3">
+                        <h3 className="font-semibold truncate">{barbershop.name}</h3>
+                        <p className="text-sm text-gray-400 truncate">{barbershop.address}</p>
+                        <Button variant="secondary" className="mt-3 w-full">Reservar</Button>
+                    </div>
+            </CardContent>
+        </Card>
+        </div>
      );
 }
  
